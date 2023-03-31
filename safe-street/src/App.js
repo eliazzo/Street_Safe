a6c7648fee95de740bec76884f5457b660aec67f
z-moz <100994664+z-moz@users.noreply.github.com>
2023-03-31 13:53:33 +0100

    map-location-pin

--- safe-street/src/App.js
+++ safe-street/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MapSection from "./components/map/Map.jsx"; // import the map here
+
+const location = {
+  crime: "Crime type",
+  lat: 37.42216,
+  lng: -122.08427,
+}; // our location object from earlier
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapSection location={location} zoomLevel={17} /> {/* include it here */}
     </div>
   );
 }
---
+++ safe-street/src/components/map/Map.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Icon } from "@iconify/react";
+import locationIcon from "@iconify/icons-mdi/map-marker";
+import "./map.css";
+
+const LocationPin = ({ text }) => (
+  <div className="pin">
+    <Icon icon={locationIcon} className="pin-icon" />
+    <p className="pin-text">{text}</p>
+  </div>
+);
+
+function MapSection(props) {
+  return (
+    <div className="map">
+      <h2 className="map-h2">Find your safe route home</h2>
+
+      <div className="google-map">
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyAvHzv00M9CQrYsgdNtPUzzXOVeXR90bvQ" }}
+          defaultCenter={props.location}
+          defaultZoom={props.zoomLevel}
+        >
+          <LocationPin
+            lat={props.location.lat}
+            lng={props.location.lng}
+            text={props.location.crime}
+          />
+        </GoogleMapReact>
+      </div>
+    </div>
+  );
+}
+
+export default MapSection
+
+
